ec331e4362e9c7dd7febcced9a536002a63c6c29
kazuto0291 <zutozuto03170291@gmail.com>
2020-05-31 14:11:11 +0900

    ReactとCssの連携

--- src/componets/App.js
+++ src/componets/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Hello, World</h1>
+      <p>こんにちは世界！</p>
     </div>
   );
 }
