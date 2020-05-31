97206f480c0cfa55fa6c5704e578145804a08bc3
kazuto0291 <zutozuto03170291@gmail.com>
2020-05-31 21:29:05 +0900

    条件に応じてコンポーネントの表示の切り替え
    render内で条件を作り、コンポーネントの表示を切り替える

--- src/componets/App.js
+++ src/componets/App.js
@@ -4,19 +4,71 @@ import Button from './Button/Button';
 import Timer from './Timer/Timer';
 import Counter from './Counter/Counter';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedId: false
+    };
+  }
+
+  login() {
+    this.setState({isLoggedId: true});
+  }
+
+  logout() {
+    this.setState({isLoggedId: false});
+  }
+
+  render() {
+    if (this.state.isLoggedId) {
+      return (
+        <div className="App">
+          <PrivatePage />
+          <button onClick={() => {this.logout()}}>ログアウト</button>
+          <h1>カウンター</h1>
+          <Counter  />
+          <h1>タイマー</h1>
+          <Timer seconds={30} />
+          <Timer seconds={50} />
+          <Button>ボタン１</Button>
+          <Button>ボタン２</Button>
+          <Button>ボタン３</Button>
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <PublicPage />
+        <button onClick={() => {this.login()}}>ログイン</button>
+      </div>
+    )
+  }
+}
+
+export default App;
+
+const PublicPage = () => {
+  const style = {
+    backgroundColor: '#ff0'
+  };
+
   return (
-    <div className="App">
-      <h1>タイマー</h1>
-      <Counter  />
-      <Timer seconds={30} />
-      <Timer seconds={50} />
-      <p>こんにちは世界！</p>
-      <Button>ボタン１</Button>
-      <Button>ボタン２</Button>
-      <Button>ボタン３</Button>
+    <div style={style}>
+      <h2>非ログイン時ページ</h2>
     </div>
   );
 }
 
-export default App;
+const PrivatePage = () => {
+  const style = {
+    backgroundColor: "#0f0"
+  };
+
+  return (
+    <div style={style}>
+      <h2>ログイン時ページ</h2>
+    </div>
+  )
+}
