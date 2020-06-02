d183fad5f770c4e5b66f2198ef18423ad683a2a4
kazuto0291 <zutozuto03170291@gmail.com>
2020-06-02 18:03:18 +0900

    onClickHandler関数の設置
    ボタンをクリックしたら実行する。

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,10 +4,19 @@ import './Button.css';
 class Button extends React.Component {
   constructor(props) {
     super(props);
+    this.onClickHandler = this.onClickHandler.bind(this);
   }
+
+  onClickHandler(event) {
+    this.props.onClickHandler(event);
+  }
+
   render() {
     return (
-      <span className="Button-container">
+      <span
+        className="Button-container"
+        onClick={this.onClickHandler}
+      >
         {this.props.children}
       </span>
     )
