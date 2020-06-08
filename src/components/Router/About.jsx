747e77614fda570633a1c8bbb3955080184036a3
kazuto0291 <zutozuto03170291@gmail.com>
2020-06-08 22:41:27 +0900

    Router-history

--- src/components/Router/About.jsx
+++ src/components/Router/About.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-function About() {
+function About(props) {
+  console.log(props)
+  const { history } = props;
   return (
     <div>
-      <h1>アバウトページです</h1>
+      <h1 onClick={() => {history.goBack();} }>アバウトページです</h1>
     </div>
   );
 }
